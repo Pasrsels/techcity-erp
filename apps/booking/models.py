449d4ba84c9fd5084170a6e2bcd5e08d45d9684f
Teddy <teddychinomona@gmail.com>
2024-11-07 16:47:46 +0200

    new fields

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -8,6 +8,7 @@ class Services(models.Model):
     name = models.CharField(max_length= 255)
     price = models.FloatField(max_length= 8)
     cost = models.FloatField(max_length= 8, blank=True)
+    measurement = models.CharField(max_length= 255)
+    recorded = models.FloatField(max_length= 8)
     date = models.DateField()
-    u_m = models.CharField(max_length= 255)
     Types = models.ForeignKey(max_length= 255, on_delete=models.CASCADE, null=True)
