32fdb6ce6660ccdad3632a0175f3206aebfd1ef2
Casper Moyo <Cassymyo@gmail.com>
2024-11-09 10:04:38 +0200

    merge

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -31,7 +31,7 @@ class Supplier(models.Model):
     contact_person = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-    address = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
