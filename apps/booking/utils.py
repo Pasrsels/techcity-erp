327a2aea33f5647e503c9eb66999f8233ad57272
Teddy <teddychinomona@gmail.com>
2024-11-11 18:21:35 +0200

    'utils'

--- apps/booking/utils.py
+++ apps/booking/utils.py
@@ -16,7 +16,7 @@ def check_admin_fee(id):
 def check_bal(id):
     member = Members.objects.filter(id= id).values('name','Member_accounts__Balance')
     member_name = member.get['Name']
-    member_bal = member.get('Member_accounts__Balance')
+    member_bal = member.get['Member_accounts__Balance']
 
     if member_bal > 0:
         return JsonResponse({'response': f'{member_name} owes ${member_bal}'})
@@ -29,5 +29,22 @@ def add_admin_fee(id):
     member_admin_fee = member.get['Payments__Admin_fee']
 
     if member_admin_fee > 0:
-        return print(f'{member_name} admin fee is {member_admin_fee}')
-    return print(f'{member_name} admin fee cleared')
+        return JsonResponse({'response':f'{member_name} admin fee is {member_admin_fee}'})
+    return JsonResponse({'response':f'{member_name} admin fee cleared'})
+
+def past_bal_payment(id):
+    member = Members.objects.filter(id= id).values('name','Payments__Admin_fee','Payments__Amount','Member_accounts__Balance')
+    cost = Members.objects.filter(id = id).values('Services__Types__Price')
+    member_name = member.get['Name']
+    member_admin_fee = member.get['Payments__Admin_fee']
+    member_payments = member.get['Payments__Amount']
+    member_bal = member.get['Member_accounts__Balance']
+    total_cost = cost.get['Services__Types__Price']
+
+    if member_admin_fee > 0:
+        remainder = member_payments - member_admin_fee
+        if remainder > 0:
+            cost = Member_accounts.Balance() 
+            cost = total_cost - remainder
+        return JsonResponse({'response': f'{cost}'})
+    
