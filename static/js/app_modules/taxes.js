dbe5fce5002cdefc187520fea812d0c3d69acd4f
shantel <shantelchikowore1@gmail.com>
2024-11-11 16:04:07 +0200

    'calculations1'

--- static/js/app_modules/taxes.js
+++ static/js/app_modules/taxes.js
@@ -3,13 +3,11 @@ function calculateInclusiveTax(price, taxRate) {
     const taxAmount = price * (taxRate / (1 + taxRate));
     return taxAmount;
 }
-
 // exclusive tax 
 function calculateExclusiveTax(price, taxRate) {
     const taxAmount = price * taxRate;
     return taxAmount;
 }
-
 module.exports = {
     calculateInclusiveTax,
     calculateExclusiveTax
