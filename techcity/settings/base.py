2bb9bdb82f208ec5df98979108537883262ca8c1
shantel <shantelchikowore1@gmail.com>
2024-11-11 09:03:55 +0200

    fixing9

--- techcity/settings/base.py
+++ techcity/settings/base.py
@@ -127,7 +127,6 @@ SESSION_AUTH = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
    'default': {
      'ENGINE': 'django.db.backends.postgresql',
        'NAME':  'TECHCITYDB',
@@ -144,24 +143,6 @@ DATABASES = {
 #          'HOST': os.getenv('DB_HOST', 'autorack.proxy.rlwy.net'),
 #          'PORT': os.getenv('DB_PORT', '26269'),
 #      }
-=======
-#    'default': {
-#      'ENGINE': 'django.db.backends.postgresql',
-#        'NAME':  'techcoty',
-#        'USER': 'postgres',
-#        'PASSWORD': 'neverfail',
-#        'HOST': 'localhost',
-#        'PORT': '5432'
-#   }
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-         'NAME': os.getenv('DB_NAME', 'railway'),
-         'USER': os.getenv('DB_USERNAME', 'postgres'),
-         'PASSWORD': os.getenv('DB_PASSWORD', 'TopCprLoVTPDAmezfOhAJoqvDuHLnxhw'),  
-         'HOST': os.getenv('DB_HOST', 'autorack.proxy.rlwy.net'),
-         'PORT': os.getenv('DB_PORT', '26269'),
-     }
->>>>>>> 380c37818373e3dc662cc48852a4416b499b73bf
  }
 
 if 'test' in sys.argv:
