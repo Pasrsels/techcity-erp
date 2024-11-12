6194751ba6349f91530d78f707bdad24681b0874
Casper Moyo <Cassymyo@gmail.com>
2024-11-12 09:41:01 +0200

    merge

--- static/js/app_modules/taxes.js
+++ static/js/app_modules/taxes.js
@@ -1,16 +1,29 @@
-// inclusive tax 
-function calculateInclusiveTax(price, taxRate) {
+// // inclusive tax 
+// function calculateInclusiveTax(price, taxRate) {
+//     const taxAmount = price * (taxRate / (1 + taxRate));
+//     return taxAmount;
+// }
+
+// // exclusive tax 
+// function calculateExclusiveTax(price, taxRate) {
+//     const taxAmount = price * taxRate;
+//     return taxAmount;
+// }
+
+// module.exports = {
+//     calculateInclusiveTax,
+//     calculateExclusiveTax
+// };
+
+///// in pure javascript we dont use, export key work
+
+
+export function calculateInclusiveTax(price, taxRate) {
     const taxAmount = price * (taxRate / (1 + taxRate));
     return taxAmount;
 }
 
-// exclusive tax 
-function calculateExclusiveTax(price, taxRate) {
+export function calculateExclusiveTax(price, taxRate) {
     const taxAmount = price * taxRate;
     return taxAmount;
 }
-
-module.exports = {
-    calculateInclusiveTax,
-    calculateExclusiveTax
-};
