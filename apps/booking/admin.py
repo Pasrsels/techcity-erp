c8d1a5b84482247ed8141396ca2035047ce47f67
Teddy <teddychinomona@gmail.com>
2024-11-27 13:11:00 +0200

    changes to models and views

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from . models import *
 
 admin.site.register(Services)
-admin.site.register(Types)
+admin.site.register(Service_product)
+admin.site.register(Service_product_details)
 admin.site.register(Members)
 admin.site.register(Member_accounts)
 admin.site.register(Payments)
