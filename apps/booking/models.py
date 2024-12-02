4379f5abfa65325efd6c1a37b5a3d2fc7e547f7e
Teddy <teddychinomona@gmail.com>
2024-12-02 15:56:18 +0200

    mn

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -52,12 +52,13 @@ class ServiceProduct(models.Model): # model titles should not have characters be
 
 class UnitMeasurement(models.Model):
     measurement = models.CharField(max_length=60, null = True)
-    promotion = models.BooleanField(default= False)
     def __str__(self):
         return f'{self.measurement}'
 
 class ServiceRange(models.Model):
     service_range = models.CharField(max_length=40)
+    service_from = models.CharField(max_length=12, null=True)
+    service_to = models.CharField(max_length=12, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     def __str__(self):
         return f'{self.service_range}'
