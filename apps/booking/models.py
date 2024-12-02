4d7a1527170ad926946a107c83db0fdd29ce0fb2
Teddy <teddychinomona@gmail.com>
2024-12-02 09:59:54 +0200

    new field description

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -37,6 +37,7 @@ class Payments(models.Model):
 
 class Services(models.Model):
     name = models.CharField(max_length= 255)
+    description = models.CharField(max_length=255)
     delete = models.BooleanField(default= False)
     def __str__(self):
         return f'{self.name}'
