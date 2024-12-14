0f015ac6ba115cdfdde154ba2afd40b2d8d625d9
Casper Moyo <114152550+casymoyo@users.noreply.github.com>
2024-12-14 10:32:25 +0200

    Revert "api view"
    This reverts commit 7f55a27e6d9ec44b1a1365ed039a1e9edc0550cd.

--- apps/pos/views.py
+++ apps/pos/views.py
@@ -9,11 +9,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from loguru import logger
 from apps.settings.models import TaxSettings
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from apps.inventory.models import Inventory
-
 
 @login_required
 @transaction.atomic
@@ -45,25 +40,3 @@ def upload_file():
     file_path = os.path.join('uploads', file.filename)
     file.save(file_path)
     return JsonResponse({"status": "success", "file_path": file_path}), 200
-
-
-#################################################################################################################################################################################################
-""" Pos API end points mainly for:
-    customers crud: check finance api views, 
-    products
-    processing the sale 
-"""
-
-class productsAPIView(APIView):
-    """ Api view which returns products """
-    def get(self, request):
-        products = Inventory.objects.all().values(
-            'id',
-            'name',
-            'price',
-            'dealer_price',
-            'category__name'
-        )
-        return Response({'success':True, 'data':products})
-    
-
