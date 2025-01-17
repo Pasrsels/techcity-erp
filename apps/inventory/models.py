40fb368d26c8d0a07d4cf658b37b3974b378de13
Teddy <teddychinomona@gmail.com>
2025-01-17 16:33:27 +0200

    NEW

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -135,7 +135,7 @@ class Inventory(models.Model):
 class Accessory(models.Model):
     main_product = models.ForeignKey(Inventory, on_delete=models.CASCADE, related_name='main_product')
     accessory_product = models.ManyToManyField(Inventory)
-    quantity = models.IntegerField()
+    #quantity = models.IntegerField()
 
     def __str__(self):
         return self.main_product.name
