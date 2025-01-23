f4484760f793e805651098d2ddd698848488487a
Casper Moyo <114152550+casymoyo@users.noreply.github.com>
2025-01-23 18:25:12 +0200

    Update views.py

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -2678,7 +2678,7 @@ def process_received_order(request):
             order_item = PurchaseOrderItem.objects.get(id=order_item_id)
 
             logger.info(order_item)
-            order = PurchaseOrder.objects.get(name=order_item.purchase_order.name)
+            order = PurchaseOrder.objects.get(id=order_item.purchase_order.id)
 
             # Update expected profit and check quantity
             cost = order_item.actual_unit_cost
@@ -2697,7 +2697,7 @@ def process_received_order(request):
 
             # Update or create inventory
             try:
-                inventory = Inventory.objects.get(id=order_item.product.id, branch=request.user.branch)
+                inventory = Inventory.objects.get(name = order_item.product.name branch=request.user.branch)
             except Product.DoesNotExist:
                 return JsonResponse({'success': False, 'message': f'Product with ID: {order_item.product.id} does not exist'}, status=404)
 
