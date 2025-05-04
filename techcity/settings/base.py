d4d285ff204fe7732ff6beeee875908cfe6d0687
Casper Moyo <casymyo@gmail.com>
2025-05-04 20:04:43 +0200

    ca

--- techcity/settings/base.py
+++ techcity/settings/base.py
@@ -159,7 +159,7 @@ DATABASES = {
 
     'default': {
          'ENGINE': 'django.db.backends.postgresql',
-         'NAME':  'techcity_db',
+         'NAME':  'techcity',
          'USER': 'postgres',
          'PASSWORD': 'neverfail',
          'HOST': 'localhost',
