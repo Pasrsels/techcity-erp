969b5d36f1ce9812709ad230028359ba9131f8bd
Casper Moyo <casymyo@gmail.com>
2025-05-11 08:20:33 +0200

    fiscal

--- techcity/settings/base.py
+++ techcity/settings/base.py
@@ -159,7 +159,7 @@ DATABASES = {
 
     'default': {
          'ENGINE': 'django.db.backends.postgresql',
-         'NAME':  'techcity_db',
+         'NAME':  'testt',
          'USER': 'postgres',
          'PASSWORD': 'neverfail',
          'HOST': 'localhost',
