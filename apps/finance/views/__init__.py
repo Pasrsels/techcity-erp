505c674f13c8827de62795cf56a2ed3cc1f644e3
Casper Moyo <cassymyo@gmai.com>
2025-05-31 15:14:29 +0200

    a

--- apps/finance/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .customer_views import *
-from .invoice_views import *
-from .expense_views import *
-from .cash_views import *
-# from .report_views import *
-# from .api_views import *
-# from .fiscal_views import * 
