75776cc4296086b336eed45f2549763970e2b041
vicky <mahunivictor13@gmail.com>
2025-07-30 14:58:22 +0200

    changes

--- techcity/settings/base.py
+++ techcity/settings/base.py
@@ -163,23 +163,23 @@ DATABASES = {
     #     default=os.environ.get('DATABASE_URL')
     # )
 
-    # 'default': {
-    #      'ENGINE': 'django.db.backends.postgresql',
-    #      'NAME':  'techcity_db',
-    #      'USER': 'postgres',
-    #      'PASSWORD': 'neverfail',
-    #      'HOST': 'localhost',
-    #      'PORT': '5432'
-    #  }
-    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'testt',
-        'USER': 'postgres',
-        'PASSWORD': 'neverfail',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME':  'techcity_db',
+         'USER': 'postgres',
+         'PASSWORD': 'neverfail',
+         'HOST': 'localhost',
+         'PORT': '5432'
+     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':  'testt',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'neverfail',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 if os.environ.get('TESTING'):
