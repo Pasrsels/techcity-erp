16ca38505796544e59ecf1dda443085ad9864cdf
chantel ntombi chikanya <chantelchikanya@outlook.com>
2025-10-06 11:00:48 +0200

    changes to purchase orders

--- apps/users/context_processor.py
+++ apps/users/context_processor.py
@@ -1,4 +1,8 @@
 from apps.users.models import User
+from apps.company.models import Branch
 
 def users(request):
     return { 'users': User.objects.filter(is_deleted=False, is_active=True)}
+
+def branches(request):
+    return { 'branches': Branch.objects.filter(disable=False)}
