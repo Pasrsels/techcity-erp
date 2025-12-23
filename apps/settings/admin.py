9f4ac4e34b7c68604217d88ad4bb0f0ddae4817f
admin <cassymyo@gmail.com>
2025-12-23 14:51:37 +0200

    new ft

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import NotificationsSettings, TaxSettings, FiscalDay, OfflineReceipt, FiscalCounter
+from .models import NotificationsSettings, TaxSettings, FiscalDay, OfflineReceipt, FiscalCounter, WhatsAppConfig
 
 admin.site.register(NotificationsSettings)
 admin.site.register(TaxSettings)
 admin.site.register(FiscalDay)
 admin.site.register(OfflineReceipt)
 admin.site.register(FiscalCounter)
-
+admin.site.register(WhatsAppConfig)
 
